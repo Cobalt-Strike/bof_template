3700b5ddbaac3acbab005e01b794426507c8db6a
Chris Thorpe <cs.christopher.thorpe@helpsystems.com>
2024-12-18 17:00:52 -0600

    Update cs_beacon_syscalls example for Cobalt Strike 4.11 release

--- examples/cs_beacon_syscalls/cs_beacon_syscalls_info.c
+++ examples/cs_beacon_syscalls/cs_beacon_syscalls_info.c
@@ -20,46 +20,45 @@ void dump_syscalls(formatp * buffer, PBEACON_SYSCALLS info) {
    }
    
    BeaconFormatPrintf(buffer, "System Calls:\n");
-   dump_api_entry(buffer, "ntAllocateVirtualMemory", &info->syscalls->ntAllocateVirtualMemory);
-   dump_api_entry(buffer, "ntProtectVirtualMemory", &info->syscalls->ntProtectVirtualMemory);
-   dump_api_entry(buffer, "ntFreeVirtualMemory", &info->syscalls->ntFreeVirtualMemory);
-   dump_api_entry(buffer, "ntGetContextThread", &info->syscalls->ntGetContextThread);
-   dump_api_entry(buffer, "ntSetContextThread", &info->syscalls->ntSetContextThread);
-   dump_api_entry(buffer, "ntResumeThread", &info->syscalls->ntResumeThread);
-   dump_api_entry(buffer, "ntCreateThreadEx", &info->syscalls->ntCreateThreadEx);
-   dump_api_entry(buffer, "ntOpenProcess", &info->syscalls->ntOpenProcess);
-   dump_api_entry(buffer, "ntOpenThread", &info->syscalls->ntOpenThread);
-   dump_api_entry(buffer, "ntClose", &info->syscalls->ntClose);
-   dump_api_entry(buffer, "ntCreateSection", &info->syscalls->ntCreateSection);
-   dump_api_entry(buffer, "ntMapViewOfSection", &info->syscalls->ntMapViewOfSection);
-   dump_api_entry(buffer, "ntUnmapViewOfSection", &info->syscalls->ntUnmapViewOfSection);
-   dump_api_entry(buffer, "ntQueryVirtualMemory", &info->syscalls->ntQueryVirtualMemory);
-   dump_api_entry(buffer, "ntDuplicateObject", &info->syscalls->ntDuplicateObject);
-   dump_api_entry(buffer, "ntReadVirtualMemory", &info->syscalls->ntReadVirtualMemory);
-   dump_api_entry(buffer, "ntWriteVirtualMemory", &info->syscalls->ntWriteVirtualMemory);
-   dump_api_entry(buffer, "ntReadFile", &info->syscalls->ntReadFile);
-   dump_api_entry(buffer, "ntWriteFile", &info->syscalls->ntWriteFile);
-   dump_api_entry(buffer, "ntCreateFile", &info->syscalls->ntCreateFile);
+   dump_api_entry(buffer, "ntAllocateVirtualMemory", &info->syscalls.ntAllocateVirtualMemory);
+   dump_api_entry(buffer, "ntProtectVirtualMemory", &info->syscalls.ntProtectVirtualMemory);
+   dump_api_entry(buffer, "ntFreeVirtualMemory", &info->syscalls.ntFreeVirtualMemory);
+   dump_api_entry(buffer, "ntGetContextThread", &info->syscalls.ntGetContextThread);
+   dump_api_entry(buffer, "ntSetContextThread", &info->syscalls.ntSetContextThread);
+   dump_api_entry(buffer, "ntResumeThread", &info->syscalls.ntResumeThread);
+   dump_api_entry(buffer, "ntCreateThreadEx", &info->syscalls.ntCreateThreadEx);
+   dump_api_entry(buffer, "ntOpenProcess", &info->syscalls.ntOpenProcess);
+   dump_api_entry(buffer, "ntOpenThread", &info->syscalls.ntOpenThread);
+   dump_api_entry(buffer, "ntClose", &info->syscalls.ntClose);
+   dump_api_entry(buffer, "ntCreateSection", &info->syscalls.ntCreateSection);
+   dump_api_entry(buffer, "ntMapViewOfSection", &info->syscalls.ntMapViewOfSection);
+   dump_api_entry(buffer, "ntUnmapViewOfSection", &info->syscalls.ntUnmapViewOfSection);
+   dump_api_entry(buffer, "ntQueryVirtualMemory", &info->syscalls.ntQueryVirtualMemory);
+   dump_api_entry(buffer, "ntDuplicateObject", &info->syscalls.ntDuplicateObject);
+   dump_api_entry(buffer, "ntReadVirtualMemory", &info->syscalls.ntReadVirtualMemory);
+   dump_api_entry(buffer, "ntWriteVirtualMemory", &info->syscalls.ntWriteVirtualMemory);
+   dump_api_entry(buffer, "ntReadFile", &info->syscalls.ntReadFile);
+   dump_api_entry(buffer, "ntWriteFile", &info->syscalls.ntWriteFile);
+   dump_api_entry(buffer, "ntCreateFile", &info->syscalls.ntCreateFile);
 
    BeaconFormatPrintf(buffer, "\nRun Time Library Functions:\n");
-   dump_rtl_entry(buffer, "rtlDosPathNameToNtPathNameUWithStatusAddr", info->rtls->rtlDosPathNameToNtPathNameUWithStatusAddr);
-   dump_rtl_entry(buffer, "rtlFreeHeapAddr", info->rtls->rtlFreeHeapAddr);
-   dump_rtl_entry(buffer, "rtlGetProcessHeapAddr", info->rtls->rtlGetProcessHeapAddr);
+   dump_rtl_entry(buffer, "rtlDosPathNameToNtPathNameUWithStatusAddr", info->rtls.rtlDosPathNameToNtPathNameUWithStatusAddr);
+   dump_rtl_entry(buffer, "rtlFreeHeapAddr", info->rtls.rtlFreeHeapAddr);
+   dump_rtl_entry(buffer, "rtlGetProcessHeapAddr", info->rtls.rtlGetProcessHeapAddr);
 }
 
 /* entry point */
 void go(char * args, int alen) {
    formatp buffer;
    int size = 0;
-   BEACON_SYSCALLS info;
+   BEACON_SYSCALLS info = {0};
    BOOL status;
-
 
    /* Allocate space for formatted output buffer */
    BeaconFormatAlloc(&buffer, 8 * 1024);
 
    /* Generate the output */
-   status = BeaconGetSyscallInformation(&info, TRUE);
+   status = BeaconGetSyscallInformation(&info, sizeof(BEACON_SYSCALLS), TRUE);
    if (status) {
       dump_syscalls(&buffer, &info);
    } else {
